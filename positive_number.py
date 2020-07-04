92bd7f7dbe7639d9c690e26a5218fabffc87e438
kr199-byte <67822701+kr199-byte@users.noreply.github.com>
2020-07-04 14:52:02 +0530

    print_positive_number
    This is for printing positive number from a given list of numbers in different manner.

---
+++ positive_number.py
@@ -0,0 +1,18 @@
+#programme to print all positive numbers
+
+list1=[12,-7,5,64,-14]
+
+for num in list1:
+    if num>=0:
+        print(num)
+
+#programme to print all positive numbers in a list.
+        
+list2=[12,14,-95,3]
+
+list_p=[]
+
+for num in list2:
+    if num>=0:
+        list_p.append(num)
+print(list_p)
